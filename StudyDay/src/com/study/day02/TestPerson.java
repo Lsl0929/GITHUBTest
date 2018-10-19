630c8800980464d9122d29e418e9c3b41971581c
Lsl0929 <605595491@qq.com>
2018-10-19 15:07:16 +0800

    类

--- StudyDay/src/com/study/day02/TestPerson.java
+++ StudyDay/src/com/study/day02/TestPerson.java
@@ -9,10 +9,14 @@ public class TestPerson {
     public static void main(String[] args) {
         //创建一个对象 Person
         //使用 new 关键字
+        //创建对象一定是调用了类的某一个构造器
+        //类中没有显示的定义构造器，则将调用默认的那个构造器
         Person p1 = new Person();
         //为对象属性赋值
         p1.name = "Tom";
+        System.out.println(p1.age);
         p1.age = 19;
+        System.out.println(p1.age);
         //调用对象的方法
         System.out.println(p1.add(2,5));
         p1.hello("Jerry");
@@ -24,6 +28,9 @@ public class TestPerson {
 
         p1.setRedius(3);
         System.out.println(p1.findArea());
+
+        Person p3 = new Person("Tam",66);
+        System.out.println(p3.name+"😍"+p3.age);
 
     }
 }
