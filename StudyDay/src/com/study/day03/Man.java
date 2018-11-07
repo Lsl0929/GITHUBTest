5bf622f714222ee2372c7e15929a33eb4e67a0ad
Lsl0929 <605595491@qq.com>
2018-11-07 14:39:31 +0800

    计算圆形面积

---
+++ StudyDay/src/com/study/day03/Man.java
@@ -0,0 +1,13 @@
+package com.study.day03;
+
+import com.study.day02.Person;
+
+/**
+ * @author 梁世龙
+ * @create 2018-11-02 11:06
+ */
+public class Man extends Person {
+    public void work(){
+        System.out.println("男人工作。。。。。");
+    }
+}
---
+++ StudyDay/src/com/study/day03/Woman.java
@@ -0,0 +1,13 @@
+package com.study.day03;
+
+import com.study.day02.Person;
+
+/**
+ * @author 梁世龙
+ * @create 2018-11-02 11:06
+ */
+public class Woman extends Person {
+    public void shopping(){
+        System.out.println("女人逛街。。。。");
+    }
+}
