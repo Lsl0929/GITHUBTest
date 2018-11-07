864bf6cfca29779387bddf37030855630ee48055
Lsl0929 <605595491@qq.com>
2018-11-07 14:39:12 +0800

    计算圆形面积

--- StudyDay/src/com/study/app/Kids1.java
+++ StudyDay/src/com/study/app/Kids1.java
@@ -18,6 +18,17 @@ public class Kids1 extends ManKind{
          somkid.yearsOld=50;
          somkid.manOrWoman();
          somkid.employee();
-         somkid.employee();
     }
+
+    /**
+     * super关键字
+     * 1.可以在子类中引用父类的成员：方法和属性。和this相对于。
+     * 2.在子类的构造方法中可以使用super(参数列表)语句调用父类的构造方法。super()调用父类默认的构造方法
+     * 3.子类的构造方法一定要调用父类的构造方法，否则编译报错
+     */
+     @Override
+     public void employee(){
+         super.employee();
+         System.out.println("不工作");
+     }
 }
