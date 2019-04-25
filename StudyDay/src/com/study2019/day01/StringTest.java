7130bff842d3c097753c8805222758a645cb97cc
Lsl <605595491@qq.com>
2019-04-25 14:25:50 +0800

    学习

---
+++ StudyDay/src/com/study2019/day01/StringTest.java
@@ -0,0 +1,15 @@
+package com.study2019.day01;
+
+/**
+ * @author Lsl
+ * @date 2019/4/24
+ * String 属于引用类型
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        String s1 = "hello word";
+        String s2 = ""; // char c= '';不能为空，必须有值，且只能放一个值，字符串可以为空
+        System.out.println(s1);
+    }
+}
