a682987f11b519abf993773c89f26da805d2d54a
Lsl0929 <605595491@qq.com>
2019-05-13 09:40:11 +0800

    面相对象

---
+++ StudyDay/src/com/study2019/day01/day02/CustomerTest.java
@@ -0,0 +1,13 @@
+package com.study2019.day01.day02;
+
+/**
+ * @author 梁世龙
+ * @create 2019-05-10 14:19
+ * 方法的声明和使用
+ *  声明  权限修饰符  返回值类型  方法名（形参列表）{   方法体  }
+ */
+public class CustomerTest {
+    public static void main(String[] args) {
+
+    }
+}
