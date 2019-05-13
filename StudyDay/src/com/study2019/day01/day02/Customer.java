616ffbced66317d271917df2034786ed4754ee5d
Lsl0929 <605595491@qq.com>
2019-05-13 10:08:38 +0800

    面相对象

---
+++ StudyDay/src/com/study2019/day01/day02/Customer.java
@@ -0,0 +1,22 @@
+package com.study2019.day01.day02;
+
+/**
+ * @author 梁世龙
+ * @create 2019-05-13 10:02
+ * 客户类
+ */
+public class Customer {
+    //属性
+    String name;
+    int age;
+
+    public void eat(){
+        System.out.println("吃饭");
+        //方法中可以调用方法
+        sleep(10);
+    }
+
+    public void  sleep(int hour){
+        System.out.println("休息了"+hour+"小时");
+    }
+}
--- StudyDay/src/com/study2019/day01/day02/CustomerTest.java
+++ StudyDay/src/com/study2019/day01/day02/CustomerTest.java
@@ -5,9 +5,15 @@ package com.study2019.day01.day02;
  * @create 2019-05-10 14:19
  * 方法的声明和使用
  *  声明  权限修饰符  返回值类型  方法名（形参列表）{   方法体  }
+ *  有返回值 vs 无返回值   根据需求
+ *  方法名 ： 属于标识符，遵循标识符的规则和规范
+ *  方法的使用：可以调用当前类的属性或方法
+ *             特殊：方法A调用了方法A： 递归方法
  */
 public class CustomerTest {
     public static void main(String[] args) {
-
+            Customer c = new Customer();
+            c.eat();
+            c.sleep(10);
     }
 }
